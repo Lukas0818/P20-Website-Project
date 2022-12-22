88cb6fba4e7a187e6beaef199b1e2b0cd29fc8f4
Lukas0818 <lukas.savickas0818@gmail.com>
2022-12-22 21:28:23 +0200

    init

---
+++ website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    name = db.Column(db.String(150))
+
+
+class Services(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    code = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Services {self.name}'
---
+++ website/views.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Services
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/services', methods=['GET', 'POST'])
+@login_required
+def services():
+    services = Services.query.all()
+    return render_template("services.html", services=services, user=current_user)
+
+
+@views.route('/photo', methods=['GET', 'POST'])
+@login_required
+def photo():
+    return render_template("photo.html", user=current_user)
+
+@views.route('/team', methods=['GET', 'POST'])
+@login_required
+def team():
+    return render_template("team.html", user=current_user)
